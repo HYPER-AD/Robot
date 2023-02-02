1e23f4355d752230b61392578ab9c52e756e0fa7
Vivek Raj <123234657+vivekraj124w@users.noreply.github.com>
2023-02-03 05:25:00 +0530

    Update anime.py

--- MukeshRobot/modules/anime.py
+++ MukeshRobot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "✰ ᴀɴɪᴍᴇ ✰"
+__mod_name__ = "✰ ᴀɴɪᴍᴇ ǫ ✰"
 __help__ = """
  ♲︎︎︎ 
 /quote :- ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
