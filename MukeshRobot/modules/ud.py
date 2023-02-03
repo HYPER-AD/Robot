baf652a9986b1559f0ad137b5bee47a21c37243e
Vivek Raj <123234657+vivekraj124w@users.noreply.github.com>
2023-02-03 06:31:48 +0530

    Update ud.py

--- MukeshRobot/modules/ud.py
+++ MukeshRobot/modules/ud.py
@@ -27,6 +27,6 @@ dispatcher.add_handler(UD_HANDLER)
 __help__ = """
 » /ud (text) *:* Searchs the given text on Urban Dictionary and sends you the information.
 """
-__mod_name__ = "Uʀʙᴀɴ🎗️"
+__mod_name__ = "✰ ᴜʀʙᴀɴ🎗 ✰"
 __command_list__ = ["ud"]
 __handlers__ = [UD_HANDLER]
