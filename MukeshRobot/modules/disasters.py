9a2bc313cac9dab2dd325c3de17559eff2abc55d
Vivek Raj <123234657+vivekraj124w@users.noreply.github.com>
2023-02-03 05:44:22 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -670,7 +670,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "ᴅᴇᴠs🔺​"
+__mod_name__ = "✰ ᴅᴇᴠs ✰"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
