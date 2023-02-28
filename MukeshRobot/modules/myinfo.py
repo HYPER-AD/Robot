44549488e813227f307eb3b38192875f2d66c5f2
Chetanthakur03 <119585273+Chetanthakur03@users.noreply.github.com>
2023-02-28 17:16:36 +0530

    Update myinfo.py

--- MukeshRobot/modules/myinfo.py
+++ MukeshRobot/modules/myinfo.py
@@ -10,13 +10,13 @@ from MukeshRobot import telethn as tgbot
 from MukeshRobot.events import register
 
 edit_time = 5
-""" =======================ᴅᴏʀᴀ ʀᴏʙᴏᴛ====================== """
+""" =======================ᴀʟᴘʜᴀ ʀᴏʙᴏᴛ====================== """
 file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
 file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
 file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
 file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
 file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
-""" =======================ᴅᴏʀᴀ ʀᴏʙᴏᴛ====================== """
+""" =======================ᴀʟᴘʜᴀ ʀᴏʙᴏᴛ====================== """
 
 
 @register(pattern="/myinfo")
@@ -65,7 +65,7 @@ async def callback_query_handler(event):
     try:
         boy = event.sender_id
         PRO = await bot.get_entity(boy)
-        LILIE = "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴡᴀʀʀɪᴏʀ ɴᴇᴛᴡᴏʀᴋ\n\n"
+        LILIE = "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛʜᴇ ᴀʟᴘʜᴀ\n\n"
         LILIE += f"ғɪʀsᴛ ɴᴀᴍᴇ: {PRO.first_name} \n"
         LILIE += f"ʟᴀsᴛ ɴᴀᴍᴇ: {PRO.last_name}\n"
         LILIE += f"ʏᴏᴜ ʙᴏᴛ : {PRO.bot} \n"
